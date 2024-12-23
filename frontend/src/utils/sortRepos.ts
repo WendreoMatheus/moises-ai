07c2129057e297b56c07f3c4a34b8afbb54ed41a
Wendreo Matheus <wendreo.pvh@gmail.com>
2024-12-23 15:04:11 -0400

    feat dockerized frontend

--- frontend/src/utils/sortRepos.ts
+++
@@ -1,9 +0,0 @@
-import { IRepo } from '@/models'
-
-export const sortRepos = (repos: Array<IRepo>, order: string): Array<IRepo> => {
-  return repos.sort((a, b) => {
-    return order === 'asc'
-      ? a.stargazers_count - b.stargazers_count
-      : b.stargazers_count - a.stargazers_count
-  })
-}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths(), svgr()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendors': ['react', 'react-dom', 'react-router'],
-
-          'state-management': ['jotai', 'swr'],
-
-          'ui-libraries': ['bulma', 'daisyui'],
-
-          utils: ['axios', 'zod'],
-
-          testing: ['jest', 'jest-environment-jsdom', 'jest-transform-stub'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 700, // Aumenta o limite para 700 kB
-  },
 })
